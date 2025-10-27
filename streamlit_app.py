f613f65402968f9edfa5688da45034ece43d2a4b
yukon47 <72011444+yukon47@users.noreply.github.com>
2025-10-27 06:40:08 +0000

    2025/10/27 write文追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write("最初の第一歩")
+st.write("Yasuo Ukon")
